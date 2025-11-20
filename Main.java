b95fc8a4b90a4f1f0ef1fc5832b9540004e014c8
Fernando <fernandobonilla832@gmail.com>
2025-11-20 11:08:18 -0300

    seteando cositas

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Holitas");
+
+        Producto prod = new Producto("pepe",  100.25);
+
+        System.out.println(prod.getName());
+    }
+}

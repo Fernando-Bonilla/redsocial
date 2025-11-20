73e9eacf12dc4717a6651b3ffa2238788db96c9e
Fernando <fernandobonilla832@gmail.com>
2025-11-20 17:54:32 -0300

    Agregado lista enlazada custom y agregamos nodos para tener una lista para mostrar de ejemlpo

---
+++ src/main/java/com/redsocial/ListaEnlazadaCustom.java
@@ -0,0 +1,56 @@
+package com.redsocial;
+
+
+public class ListaEnlazadaCustom {
+
+    private NodoEnlazadoCustom head;
+    private NodoEnlazadoCustom ultimo;
+
+    public void AgregarNodo(NodoEnlazadoCustom nodo){
+
+        if(head == null){
+            this.head = nodo;
+            this.ultimo = nodo;
+        }
+
+        ultimo.setSiguiente(nodo);
+        ultimo = nodo;
+        
+    }
+
+    public void ImprimirList(){
+        ImprimirNodo(head);
+    }
+
+    public void ImprimirNodo(NodoEnlazadoCustom nodo){
+
+        System.out.println(nodo.getId());
+
+        if(nodo.getSiguiente() != null){
+
+            ImprimirNodo(nodo.getSiguiente());
+        }
+        
+    }
+
+    public String ListarUsuarios(){
+        if (head == null) {
+            return "";
+        }
+        return MostrarNodo(head, "");
+    }
+
+    public String MostrarNodo(NodoEnlazadoCustom nodo, String listaAcumulada){
+
+        listaAcumulada += (nodo.getId()) + ", ";
+
+        if(nodo.getSiguiente() != null){
+
+            return MostrarNodo(nodo.getSiguiente(), listaAcumulada);
+        }else{
+           return listaAcumulada;
+        }
+        
+    }
+    
+}
--- src/main/java/com/redsocial/UsuariosResource.java
+++ src/main/java/com/redsocial/UsuariosResource.java
@@ -5,20 +5,43 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("hello")
+@Path("usuarios")
 public class UsuariosResource {
-
+    
     @GET
+    @Path("hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         UsuariosRepositorio usuarioR = UsuariosRepositorio.getInstance();
         
         int cont = usuarioR.sumarContador();
 
+        ListaEnlazadaCustom listaCustom = new ListaEnlazadaCustom();
+
+        for(int i = 1; i < 6; i++){
+            NodoEnlazadoCustom nodo = new NodoEnlazadoCustom(i);
+            listaCustom.AgregarNodo(nodo);
+        }
+        
+        listaCustom.ImprimirList();
+
         return "Hola JAX-RS con Jersey + Grizzly: " + cont;
     }
-    
 
+    @GET
+    @Path("listar")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String ListarUsuarios(){
+
+        ListaEnlazadaCustom listaCustom = new ListaEnlazadaCustom();
+
+        for(int i = 1; i < 6; i++){
+            NodoEnlazadoCustom nodo = new NodoEnlazadoCustom(i);
+            listaCustom.AgregarNodo(nodo);
+        }
+
+        return listaCustom.ListarUsuarios();
+    }
 
 
 }

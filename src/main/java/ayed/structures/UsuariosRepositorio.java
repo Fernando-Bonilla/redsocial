1525fe42f76d2b2dac11e1f40656e2a277c7cb24
Juan Lluviera <juanlluviera03@gmail.com>
2025-11-27 22:11:44 -0300

    agrega tablaHash al UsuarioRespositorios para almacenar el grafoUsuario con busqueda de O(1) promedio, con manejo de colisiones

--- src/main/java/ayed/structures/UsuariosRepositorio.java
+++ src/main/java/ayed/structures/UsuariosRepositorio.java
@@ -3,9 +3,12 @@ package ayed.structures;
 public class UsuariosRepositorio {    
     public static UsuariosRepositorio _instance;
     private UsuariosListaEnlazada _usuarios;
+    private TablaHash<Integer, NodoUsuarioGrafo> grafoUsuarios;
+    private int contadorIds = 0;
 
     private UsuariosRepositorio(){
         this._usuarios = new UsuariosListaEnlazada();
+        this.grafoUsuarios = new TablaHash<>();
     }
 
     public UsuariosListaEnlazada getUsuarios(){
@@ -18,5 +21,21 @@ public class UsuariosRepositorio {
         }
 
         return _instance;
+    }
+
+    public TablaHash<Integer, NodoUsuarioGrafo> getGrafoUsuarios() {
+        return grafoUsuarios;
+    }
+
+    public int generarIdUsuario(){
+        return ++contadorIds;
+    }
+
+    public void guardarEnMemoria(){
+        //Logica para serializar y guardar en un archivo local
+    }
+
+    public void cargarDeMemoria(){
+        //Logica para cargar el archivo local y deserializar
     }
 }
